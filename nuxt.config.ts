b4201bc5167f4a01f6efcc3404f9b2d8fe27f88a
Vladimir <pestov.web@yandex.ru>
2023-03-09 21:01:36 +0300

    feat: social btn, head cfg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=500, initial-scale=1",
+      title: "DVGames",
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: "возрождение портала", content: "портал двгеймс." },
+      ],
+    },
+  },
   css: [
     "vuetify/lib/styles/main.sass",
     "@mdi/font/css/materialdesignicons.min.css",
